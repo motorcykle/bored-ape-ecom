c00fa97223b2393d7ed3b8c29772767500f732b8
motorcykle <mohamedabdulridha1@gmail.com>
2022-01-02 13:19:40 +0100

    setup

---
+++ data/apes.ts
@@ -0,0 +1,5 @@
+const apes = [
+
+]
+
+export default apes;
---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+import apes from '../data/apes';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.feedback.createMany({
+    data: apes
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect);
