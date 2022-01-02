b29c5e31c140907f33209c1402dd1301a54fa87d
motorcykle <mohamedabdulridha1@gmail.com>
2022-01-02 14:03:09 +0100

    fixing apes.ts

--- data/apes.ts
+++ data/apes.ts
@@ -1,5 +1,54 @@
-const apes = [
+import { Ape } from "@prisma/client";
 
+const apes: Ape[] = [
+    {
+        id: "ckxx96ada000109l5h1etacn9",
+        number: 3368,
+        current_owner: "ddr_vault",
+        price: 420,
+        likes_opensea: 2100,
+        image_url: "https://lh3.googleusercontent.com/u-2FnHbaJ3U_KCDlmg2McX9Yfo7brsAzOffqihNXCGkHljA89SPPzwdjQiVSWcsvxCoj_ydBcDNCuZvHEekaYekaMEH4XX32k9US=w542"
+    },
+    {
+        id: "ckxx97alx000209l58zvb7zek",
+        number: 529,
+        current_owner: "StrongHandz",
+        price: 178.95,
+        likes_opensea: 262,
+        image_url: "https://lh3.googleusercontent.com/xK7DprIx_ffBEJBVayZcpcyE4a4By8UmPDIcjH7E3B75qO8AijMX1WLsqa0-0SUIKUObHtKRT_vX_KeadeZi929pEP-Rp0wGFsv0WA=w542"
+    },
+    {
+        id: "ckxx97hxh000309l50txb5vxl",
+        number: 7561,
+        current_owner: "MatrixxSafe",
+        price: 96.69,
+        likes_opensea: 111,
+        image_url: "https://lh3.googleusercontent.com/Whjpjv1pao0aYYWSBlSs6gzduoFepAaypR0P6cRindkEG8oRayJPRm6mVs26JyXklyemljY8qh1cya-wgB6ZMbKbhYQPBvZFOKlQbOc=w542"
+    },
+    {
+        id: "ckxx97lr8000409l51slk76jp",
+        number: 5933,
+        current_owner: "003242059x329429xx93293",
+        price: 78,
+        likes_opensea: 621,
+        image_url: "https://lh3.googleusercontent.com/7z2jr2l2aCNFVWm18qpJ0UEZ5nHycUWGiqhixYD6RH2Jt8acY_hBRYPfGxQOY6U6A98duNhFZa-aQmfpcXwoaqvzzqsRAS3bKvNtUg=w542"
+    },
+    {
+        id: "ckxx97q0y000509l5gp8m2npc",
+        number: 5983,
+        current_owner: "13030347c65716673636x0",
+        price: 100.269,
+        likes_opensea: 207,
+        image_url: "https://lh3.googleusercontent.com/TXCZTh7iQOP-nyVBq83iCc2o-Qz9P_xrBVOigteCXt85ExjuSYr7YK24lnQnj9Nnsqid2njH8SxbzeqEf6Epe9Md2VN9D26gDMh2=w542"
+    },
+    {
+        id: "ckxx97w9n000609l52bh2gmhw",
+        number: 2261,
+        current_owner: "D8539B",
+        price: 0.3647,
+        likes_opensea: 29,
+        image_url: "https://lh3.googleusercontent.com/JyQTUlISmzK07Ud9yH2BGkqEla6YzyZam0OV_R2LLcby486aW_Dk7oZ4mw5D3zCNN_l6N8orxgtBFOBpI-d1NDSMfMABWtBUkfH62XI=w542"
+    }
 ]
-
+    
 export default apes;
