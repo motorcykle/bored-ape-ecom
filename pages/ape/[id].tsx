4abda6e62cc001e9891d34020522a616f472fd89
motorcykle <mohamedabdulridha1@gmail.com>
2022-01-02 22:51:04 +0100

    pagedone

--- pages/ape/[id].tsx
+++ pages/ape/[id].tsx
@@ -13,19 +13,27 @@ export default function ApePage({ ape }: { ape: Ape }) {
 
     return (
         <div className='p-3'>
+            <div className="md:max-w-7xl mx-auto grid gap-3 grid-cols-1 md:grid-cols-2">
                 <img src={image_url} alt="" />
-                <div className='flex justify-between'>
-                    <h2 className='text-2xl font-bold text-zinc-900'>#{number}</h2> 
+                <div className="flex flex-col space-y-3 items-start w-full">
+                    <div className='flex justify-between w-full'>
+                        <h2 className='text-4xl font-bold text-zinc-900'>#{number}</h2> 
+                        <div className="flex items-center space-x-1">
+                            <Image src={WethIcon} alt='' width={20} height={20}/>
+                            <p className='font-medium text-zinc-800'>{price.toString().replace('.', ',')}</p>
+                        </div> 
+                    </div>
+                    
                     <div className="flex items-center space-x-1">
-                        <Image src={WethIcon} alt='' width={20} height={20}/>
-                        <p className='font-medium text-zinc-800'>{price.toString().replace('.', ',')}</p>
-                    </div> 
-               </div>
-               
-               <div className="flex items-center space-x-1">
-                    <HeartIcon className='h-5 w-5 text-zinc-400' />
-                    <p className=' font-normal text-zinc-500'>{kFormatter(likes_opensea)}</p>
-               </div>
+                        <HeartIcon className='h-5 w-5 text-zinc-400' />
+                        <p className=' font-normal text-zinc-500'>{kFormatter(likes_opensea)}</p>
+                    </div>
+
+                    <p className=''>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis accusamus earum optio eligendi? Dolore ipsam dolorem illum dicta placeat ratione sit porro. Sapiente, optio. Optio accusamus molestiae aperiam quos nesciunt!</p>
+
+                    <button className=' text-neutral-50 tracking-wide text-xl font-medium bg-black rounded-lg py-2 px-4'>Add to cart</button>
+                </div>
+            </div>
         </div>
     )
 }
